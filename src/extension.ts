588e62ff55be0f845db761afb9e2efdde13c4757
Amir Hoseinian <amir.hoseinian@aboutyou.de>
2018-12-09 19:23:12 +0100

    Remove extra stuff

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,8 @@ import * as vscode from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Region folder is ready to go!!!");
-  });
-
   const changedVisibleTextEditorsListener = vscode.window.onDidChangeVisibleTextEditors(
-    editors => {
+    () => {
       const tab = vscode.window.activeTextEditor;
       if (tab) {
         vscode.commands.executeCommand(`editor.foldAllMarkerRegions`);
@@ -28,7 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(disposable, changedVisibleTextEditorsListener);
+  context.subscriptions.push(changedVisibleTextEditorsListener);
 }
 
 // this method is called when your extension is deactivated
